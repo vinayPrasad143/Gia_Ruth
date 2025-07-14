c29e495e4b481344edfefe5f7ecae0d63028fbab
vinayprasad.testy <vinayprasad.testy@gmail.com>
2025-07-14 13:58:44 +0530

    made changes in the Testrunner file

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
         features = "ruthfeatures",
         glue = "ruthstepdef",
         dryRun = false,
-        tags = "@login",
+        tags = "@system_test",
         plugin = { "pretty", // Human-readable console output
                 "html:target/cucumber-html-reports/overview-features.html", // Simple HTML report (optional)
                 "json:target/cucumber-json/cucumber.json", // Needed for Jenkins' Cucumber plugin
